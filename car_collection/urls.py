524745686b248afd60226176e339a915d8cb9c27
Beatris Ilieve <beatrisilieve@icloud.com>
2023-09-01 14:50:18 +0300

    APPS; URLS
    - Started `account`, `car` and `common` apps,
    - Added the app names to `INSTALLED APPS` into `settings.py`
    - Created `urls.py` files with empty urlpatterns
    - Included `urls.py` files into project's  `urls.py`

--- car_collection/urls.py
+++ car_collection/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('car_collection.common.urls')),
+    path('account/', include('car_collection.account.urls')),
+    path('car/', include('car_collection.car.urls')),
 ]
