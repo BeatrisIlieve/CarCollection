7599ebe4031a6e92c76b95060a07d18e3e1550eb
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-01 22:13:43 +0300

    Added cache decorator to car `delete view`

--- car_collection/common/views.py
+++ car_collection/common/views.py
@@ -1,16 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views.decorators.cache import cache_page
-
-from car_collection.account.models import Profile
-
-
 
 @cache_page(15 * 60)
 def index(request):
-    cars = CarLookedAt.objects.all()
-
-    context = {
-        'cars': cars,
-    }
-
-    return render(request, 'common/index.html', context)
+    return render(request, 'common/index.html')
