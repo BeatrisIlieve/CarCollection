1885479d369c400612617df20ab034e85c031834
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-01 21:53:34 +0300

    Deleted model CarLookedAt

--- car_collection/common/views.py
+++ car_collection/common/views.py
@@ -4,20 +4,13 @@ from django.views.decorators.cache import cache_page
 from car_collection.account.models import Profile
 
 
-# def get_profile():
-#     try:
-#         return Profile.objects.get()
-#     except Profile.DoesNotExist as ex:
-#         return None
-#
-#
-# def index(request):
-#     profile = get_profile()
-#
-#     if profile is None:
-#         return render(request, 'common/index.html')
-#     return render(request, 'common/index.html')
 
 @cache_page(15 * 60)
 def index(request):
-    return render(request, 'common/index.html')
+    cars = CarLookedAt.objects.all()
+
+    context = {
+        'cars': cars,
+    }
+
+    return render(request, 'common/index.html', context)
