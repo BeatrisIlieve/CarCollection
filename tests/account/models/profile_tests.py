f952c4732048f8c964ad4ddf64ef1d22d7b1c978
Beatris Ilieve <beatrisilieve@icloud.com>
2023-10-04 16:16:34 +0300

    Added Unit Test to check if ValidationError when first name min length is invalid

--- tests/account/models/profile_tests.py
+++ tests/account/models/profile_tests.py
@@ -1,10 +1,11 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from car_collection.account.models import Profile, CarCollectionUser
 
 
 class ProfileModelTests(TestCase):
-    def test_profile_save__when_first_name_is_valid__expect_corrct_result(self):
+    def test_profile_save__when_first_name_min_length_is_valid__expect_corrct_result(self):
         user = CarCollectionUser(
             email='beatrisilieve@icloud.com',
             password='S@3ana3a'
@@ -24,3 +25,25 @@ class ProfileModelTests(TestCase):
         profile.save()
 
         self.assertIsNotNone(profile.user_id)
+
+    def test_profile_save__when_first_name_min_length_is_invalid__expect_corrct_result(self):
+        user = CarCollectionUser(
+            email='beatrisilieve@icloud.com',
+            password='S@3ana3a'
+        )
+
+        user.full_clean()
+        user.save()
+
+        profile = Profile(
+            first_name='B',
+            last_name='Ilieve',
+            user=user,
+
+        )
+
+        with self.assertRaises(ValidationError) as ve:
+            profile.full_clean()
+            profile.save()
+
+        self.assertIsNotNone(ve)
